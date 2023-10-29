799c12855503a24aea3c4584450eb12ea2dfa268
Jen0234 <JennaLafable@gmail.com>
2023-10-29 04:29:16 -0400

    Update page/javascript
    -add timeslots in html
    - stylesheet
    - time
    - date

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,20 @@ function updateClock() {
 }
 
 updateClock();
+
+//change the time  slot  to the current time
+
+const past = document.getElementsByClassName('past');
+const present = document.getElementsByClassName('present');
+const future = document.getElementsByClassName('future');
+const timeblock = document.getElementsByClassName('time-block');
+
+arrT = [past, present, future];
+
+
+function changetimeblock() {
+
+   
+}
+
+console.log(arrT);
